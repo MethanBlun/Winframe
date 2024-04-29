45947afb1ebf35c5f0570989aa57445ee83f08f4
Osirus Jack <methanblun@gmail.com>
2024-04-29 18:58:35 +0000

    improve quotes style

--- src/quoteGenerator.jsx
+++ src/quoteGenerator.jsx
@@ -1,22 +1,19 @@
-
 import React from "react";
 import { useState } from "react";
 
+function Quote() {
+  const [quote, setQuote] = useState("    life is grim");
+  // const quotes = [{
+  //     quote1 :'life is better with a cat'
+  // },{
+  //     quote2 : 'dont have conversation with idiot in the morning'
+  // }]s
 
-
-
-function Quote(){
-    const [quote,setQuote] = useState('life is grim')
-    // const quotes = [{
-    //     quote1 :'life is better with a cat'
-    // },{
-    //     quote2 : 'dont have conversation with idiot in the morning'
-    // }]
-   
-    
-    return (
-        <div className="flex justify-center bg-red-100">{quote}</div>
-    )
-
+  return (
+    <div className="flex justify-center bg-slate-700 p-5 ">
+      <div className=" text-slate-100">citation du jour :</div>
+      <div>{quote}</div>
+    </div>
+  );
 }
 export default Quote;

1f0023e0dbf864e71e530277c07113bb08c29cc1
Osirus Jack <methanblun@gmail.com>
2024-04-29 18:17:05 +0000

    delimitation mistake fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 const App = () => {
   return (
-    <div className="relative h-screen">
-      {/* Première partie */}
-      <div className="absolute top-0 left-0 h-full w-1/4 bg-red-300">dashbord</div>
-
-      {/* Deuxième partie */}
-      <div className="absolute top-0 right-0 h-1/3 w-3/4 bg-blue-300">c</div>
-
-      {/* Troisième partie */}
-      <div className="absolute top-1/4 left-1/3 w-2/3 h-2/4 bg-cyan-300">task manager</div>
-
-      {/* Footer */}
+    <div className="relative h-screen w-full">
+      <div className="absolute  top-0 left-0 h-full w-1/5 bg-red-300">
+        dashbord
+      </div>
+      <div className="absolute top-0 right-0 h-1/4 w-4/5 bg-blue-300">
+        Quote area
+      </div>
+      <div className="absolute bottom-0 right-0 h-4/5 w-4/5 bg-cyan-300">
+        task area
+      </div>
+      task area
       <footer className="fixed bottom-0 left-0 w-full bg-gray-500">
         <div className="container mx-auto py-4">
           <p className="text-center text-white">Footer</p>

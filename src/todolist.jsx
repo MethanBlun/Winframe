5ca99390b4c9e3d615486bfd199f26c30e8ce093
Osirus Jack <methanblun@gmail.com>
2024-04-30 16:42:56 +0000

    better design input and form

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -3,15 +3,15 @@ import {Button} from "@nextui-org/react";
 import {Input} from "@nextui-org/react";
 export default function Todolist() {
   return (
-    <div className=" flex items-start justify-center bg-slate-400 h-full w-full">
-      <form className="p-9 flex flex-row gap-3" onSubmit={null}>
+    <div className=" flex  items-start justify-center bg-slate-400 h-full w-full">
+      <form className="p-7 m-5 w-full flex flex-row border-3 border-slate-900 rounded-xl h-3 gap-3" onSubmit={null}>
         <div>
-          <label className=" p-2 text-slate-100" htmlFor="item"></label>
+          <label className=" text-slate-100" htmlFor="item"></label>
 
-            <Input type="text"></Input>
+            <Input className="w-full "  type="text"></Input>
         </div>
 
-        <Button  className="h-7  bg-teal-800"> Add </Button>
+        <Button  className="h-10 bg-slate-200  "> Add </Button>
       </form>
     </div>
   );

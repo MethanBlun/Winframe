054306267b068ad39d7975171469bfdf478da70d
Osirus Jack <methanblun@gmail.com>
2024-04-30 16:48:30 +0000

    rebeuil common div of input and button

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import {Button} from "@nextui-org/react";
-import {Input} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 export default function Todolist() {
   return (
     <div className=" flex  items-start justify-center bg-slate-400 h-full w-full">
-      <form className="p-7 m-5 w-full flex flex-row border-3 border-slate-900 rounded-xl h-3 gap-3" onSubmit={null}>
+      <form
+        className="p-7 m-5 w-full  border-3 border-slate-900 rounded-xl h-3 "
+        onSubmit={null}
+      >
         <div>
-          <label className=" text-slate-100" htmlFor="item"></label>
+          <div>
+            <label className=" text-slate-100" htmlFor="item"></label>
 
-            <Input className="w-full "  type="text"></Input>
+            <Input className="w-90 " type="text"></Input>
+          </div>
+
+          <Button className="h-10 bg-slate-200  "> Add </Button>
         </div>
-
-        <Button  className="h-10 bg-slate-200  "> Add </Button>
       </form>
     </div>
   );

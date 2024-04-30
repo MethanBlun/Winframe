5fcb7421e2042e27ae9ea77d6f2136fac1575bb5
Osirus Jack <methanblun@gmail.com>
2024-04-30 16:56:14 +0000

    applied flex-Grow to the input and all solved

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
+
 export default function Todolist() {
   return (
-    <div className=" flex  items-start justify-center bg-slate-400 h-full w-full">
+    <div className="flex items-start justify-center bg-slate-400 h-full w-full">
       <form
-        className="p-7 m-5 w-full  border-3 border-slate-900 rounded-xl h-3 "
+        className="p-7 m-5 w-full border-3 border-slate-900 rounded-xl"
         onSubmit={null}
       >
-        <div>
-          <div>
-            <label className=" text-slate-100" htmlFor="item"></label>
-
-            <Input className="w-90 " type="text"></Input>
+        <div className="flex items-center justify-between">
+          <label className="text-slate-100" htmlFor="item"></label>
+          <div className="flex-grow">
+            <Input className="w-full" type="text"></Input>
           </div>
-
-          <Button className="h-10 bg-slate-200  "> Add </Button>
+          <Button className="h-10 bg-slate-200"> Add </Button>
         </div>
       </form>
     </div>

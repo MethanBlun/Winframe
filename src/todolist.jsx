0d9c70ce28004966772c43ffe75adf0e9e53ef01
Osirus Jack <methanblun@gmail.com>
2024-04-30 15:54:49 +0000

    beggin building the todo generatores Style

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,10 +1,21 @@
+import React from "react";
 
-import React from "react"
+export default function Todolist() {
+  return (
+    <div  className=" flex items-start justify-center bg-slate-400 h-full w-full">
+      <form className="   p-9  " onSubmit={null}>
+        <div>
+          <label htmlFor="item">New Item</label>
+          <input
+          //   value={newItem}
+          //   onChange={e => setNewItem(e.target.value)}
+          //   type="text"
+          //   id="item"
+          />
+        </div>
 
-export default function Todolist(){
-    return(
-        <div className=" bg-slate-400 h-full w-full"> </div>
-    )
-      
-
+        <button className="  ">Add</button>
+      </form>
+    </div>
+  );
 }

35253483acced0af683feb65ebd126cb743429e2
Osirus Jack <methanblun@gmail.com>
2024-05-02 04:51:52 +0000

    the ul who will contain tasks work

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,7 +5,6 @@ import { Input } from "@nextui-org/react";
 export default function Todolist() {
   const [task, setTask] = useState("");
 
-
   function handleInputChange(event) {
     const value = event.target.value;
 
@@ -16,30 +15,30 @@ export default function Todolist() {
     console.log(task);
   }
   return (
-    <div className="h-full bg-orange-600">
-
-   
-    <div className="flex items-start justify-center bg-slate-400 h-/4 w-full">
-      <form
-        className="p-7 m-5 w-full border-3 border-slate-900 rounded-2xl"
-        onSubmit={null}
-      >
-        <div className="flex gap-3 h-2 items-center justify-between">
-          <div className="flex-grow">
-            <Input
-              className="w-full"
-              onChange={handleInputChange}
-              type="text"
-            ></Input>
+    <div className="h-full flex flex-col bg-orange-600">
+      <div className="flex items-start justify-center bg-slate-400 h-/4 w-full">
+        <form
+          className="p-7 m-5 w-full border-3 border-slate-900 rounded-2xl"
+          onSubmit={null}
+        >
+          <div className="flex gap-3 h-2 items-center justify-between">
+            <div className="flex-grow">
+              <Input
+                className="w-full"
+                onChange={handleInputChange}
+                type="text"
+              ></Input>
+            </div>
+            <Button onClick={handleButtonClick} className="h-10 bg-slate-200">
+              {" "}
+              Add{" "}
+            </Button>
           </div>
-          <Button onClick={handleButtonClick} className="h-10 bg-slate-200">
-            {" "}
-            Add{" "}
-          </Button>
-        </div>
-      </form>
-      
+        </form>
+      </div>
+      <ul className=" bg-slate-100 w-full h/4/5">
+
+      </ul>
     </div>
-     </div>
   );
 }

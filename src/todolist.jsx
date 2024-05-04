65557c190c40e425e4da656887c4c8d068abe22c
Osirus Jack <methanblun@gmail.com>
2024-05-04 12:48:43 +0000

    delete task logic works

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,46 +5,6 @@
 // export default function Todolist() {
 //   const [task, setTask] = useState("");
 
-//   function handleInputChange(event) {
-//     const value = event.target.value;
-//     setTask(value);
-//   }
-
-//   function handleButtonClick() {
-//     console.log(task);
-//   }
-//   return (
-//     <div className="h-full flex flex-col">
-//       <div className="flex items-start justify-center bg-slate-400 h-/4 w-full">
-//         <form
-//           className="p-7 m-5 w-full border-3 border-slate-900 rounded-2xl"
-//           onSubmit={(event) => {
-//             event.preventDefault(); // forbed the refresh of page after submit
-//             handleButtonClick();
-//           }}
-//         >
-//           <div className="flex gap-3 h-2 items-center justify-between">
-//             <div className="flex-grow">
-//               <Input
-//                 className="w-full"
-//                 onChange={handleInputChange}
-//                 type="text"
-//               ></Input>
-//             </div>
-//             <Button onClick={handleButtonClick} className="h-10 bg-slate-200">
-//               {" "}
-//               Add{" "}
-//             </Button>
-//           </div>
-//         </form>
-//       </div>
-//       <ul className=" bg-slate-100 w-full h/4/5">
-//         <li>hit the road jack</li>
-//       </ul>
-//     </div>
-//   );
-// }
-
 import React, { useState } from "react";
 import { Button } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
@@ -67,16 +27,13 @@ export default function Todolist() {
     }
   }
   function handleDelete(index) {
-    // setTasks((prevTasks) => {
-    //   return prevTasks.filter((t) => t.id !== id);
-    //   //comment supprimer une tache specifique parmis des taches
-    //   //est ce que je gere bien les id?
-    
-    // });
-
+    console.log(index);
+    setTasks((prevTasks) => {
+      return prevTasks.filter((_t, tIndex) => tIndex !== index);
+    });
   }
-  function handleDone(){
-    alert(`congratulation tu as achever 1 tache sur ${tasks.length}`)
+  function handleDone() {
+    alert(`congratulation tu as achever 1 tache sur ${tasks.length}`);
   }
 
   return (
@@ -106,7 +63,7 @@ export default function Todolist() {
         </form>
       </div>
       <ul className=" flex flex-col-reverse p-2 w-full h/4/5">
-        {tasks.map((task,index) => (
+        {tasks.map((task, index) => (
           <div className="flex gap-1" key={index}>
             <li
               className=" border-1 p-2  m-2 rounded-lg  w-4/5 text-center border-black"
@@ -115,14 +72,17 @@ export default function Todolist() {
               {task}
             </li>
             {
-              <button onClick={handleDone}
-              className=" border p-2  m-2 border-black bg-green-400 rounded-lg">
+              <button
+                onClick={handleDone}
+                className=" border p-2  m-2 border-black bg-green-400 rounded-lg"
+              >
                 done
               </button>
             }
             {
               <button
-                onClick={handleDelete}
+                onClick={() => handleDelete(index)}
+                //it may happend an infinity loop if i dont pass index indisde of arrow function
                 className=" border p-1  m-2 border-black bg-red-600 rounded-lg"
               >
                 Delete
@@ -134,3 +94,5 @@ export default function Todolist() {
     </div>
   );
 }
+//comment supprimer une tache specifique parmis des taches
+//est ce que je gere bien les id?

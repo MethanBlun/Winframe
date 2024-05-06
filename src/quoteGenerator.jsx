a2c6ac4c804502027004768290c64db9bf0dc67f
Osirus Jack <methanblun@gmail.com>
2024-05-06 14:43:58 +0000

    select random quote

--- src/quoteGenerator.jsx
+++ src/quoteGenerator.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-//chercher comment extraire des donnees json et le afficher en react
+
 //faire le style
 //mettre une boutons hidden en cas de besoins de switch
 
@@ -20,7 +20,9 @@ function Quote() {
       try {
         const response = await fetch("./learning.json");
         const json = await response.json();
-        setQuote(json[4].quote);
+        const rnd = Math.floor(Math.random() * json.length)
+        setQuote(json[rnd].author);
+        console.log(quote)
       } catch (error) {
         console.error("Erreur lors de la récupération des citations :", error);
       }
@@ -35,5 +37,36 @@ function Quote() {
     </div>
   );
 }
+
+
+
+
+// fetch('quotes.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     // Une fois que le fichier est chargé avec succès, vous pouvez utiliser son contenu
+//     var citations = data;
+
+//     // Fonction pour afficher une citation et son auteur au hasard
+//     function afficherCitationAuHasard() {
+//         // Sélectionner une citation au hasard
+//         var citation = citations[Math.floor(Math.random() * citations.length)];
+//         // Afficher la citation et son auteur
+//         console.log('Citation: ' + citation.quote);
+//         console.log('Auteur: ' + citation.author);
+//     }
+
+//     // Appel de la fonction pour afficher une citation au hasard
+//     afficherCitationAuHasard();
+//   })
+//   .catch(error => {
+//     // En cas d'erreur lors du chargement du fichier JSON
+//     console.error('Une erreur est survenue lors du chargement du fichier JSON :', error);
+//   });
+
+
+
+
+
 
 export default Quote;

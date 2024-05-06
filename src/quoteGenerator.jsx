e612d6843dfe02afaf30a586cd3e565416148823
Osirus Jack <methanblun@gmail.com>
2024-05-06 13:53:05 +0000

    fetch data from json finaly work , je isssue was putting json file on src , it may puted on public

--- src/quoteGenerator.jsx
+++ src/quoteGenerator.jsx
@@ -12,37 +12,22 @@ import { useState, useEffect } from "react";
 //comment faire le mode sombre en tailwind
 //ou vais je mettre le boutton pour changer de theme
 
-// function Quote() {
-//   const [quote, setQuote] = useState("hello world");
-
-  
-  
-//   return (
-//     <div className="flex justify-center h-full p-10  w-full bg-slate-700  ">
-//       <div className=" text-zinc-300"></div>
-//       <div className="text-zinc-300">{quote}</div>
-//     </div>
-//   );
-// }
-// export default Quote;
-
-
-// import React, { useState, useEffect } from "react";
-
 function Quote() {
   const [quote, setQuote] = useState("hello world");
 
   useEffect(() => {
-    fetch(`learning.json`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        const allquotes = data;
-        console.log(allquotes);
-      })
-      .catch((error) => {
-        console.error("Erreur lors de la récupération du JSON :", error);
-      });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./learning.json");
+        const json = await response.json();
+        setQuote(json[4].quote);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des citations :", error);
+      }
+    };
+
+    fetchData();
+  }, []); 
 
   return (
     <div className="flex justify-center h-full p-10 w-full bg-slate-700">

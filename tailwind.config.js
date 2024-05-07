b1b1035c3cacb33c2e29468bde29c05edb58a9d0
Osirus Jack <methanblun@gmail.com>
2024-05-07 16:22:36 +0000

    begin building responsive ui with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
-const {nextui} = require('@nextui-org/theme');
+const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+    },
     extend: {},
   },
   darkMode: "class",
- plugins: [nextui()],
-}
-
+  plugins: [nextui()],
+};
